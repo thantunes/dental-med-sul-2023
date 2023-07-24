5f2bf8fb8176a8c206f8d033aa2fd44af7a67993
guilhermeevision <guilherme@evision.com.br>
2023-07-24 15:28:41 -0300

    Fix

--- tdzaindevops-dental-med-sul-io-a37ff3999229/react-app-template-master/react/components/PriceDiscount/index.tsx
+++ tdzaindevops-dental-med-sul-io-a37ff3999229/react-app-template-master/react/components/PriceDiscount/index.tsx
@@ -4,7 +4,7 @@ import './index.global.css'
 type props = {
     children: any
 }
-const PriceDiscount: FC<props> = ({children}) => {
+const PriceDiscount: FC<props> = ({ children }) => {
     const [ItemSelected, setItemSelected] = useState(0)
     useEffect(function () {
         const Selec: any = document.getElementsByClassName('ba mb3 pa5 vtex-sku-list-1-x-skuContentWrapper vtex-sku-list-1-x-skuContentWrapper--skuListContent bw1 b--blue vtex-sku-list-1-x-selectedSkuContentWrapper vtex-sku-list-1-x-selectedSkuContentWrapper--skuListContent')
@@ -19,17 +19,27 @@ const PriceDiscount: FC<props> = ({children}) => {
     //     Produto: productContextValue?.product,
     //     Name: productContextValue?.product?.productName,
     // })
-    console.log('Contexto de produto',productContextValue)
+    console.log('Contexto de produto', productContextValue)
     const value = productContextValue?.product?.items[ItemSelected].sellers[0].commertialOffer?.Price == undefined ? 0 : productContextValue?.product?.items[ItemSelected].sellers[0].commertialOffer?.Price
     // const listValue = productContextValue?.product?.items[ItemSelected].sellers[0].commertialOffer?.Price == undefined ? 0 : productContextValue?.product?.items[ItemSelected].sellers[0].commertialOffer?.ListPrice
     // const CalcPrice = (value - (value * 0.03))
     return (
         <>
             <div>
-                <p className='vtex-product-price-1-x-sellingPriceValue' style={{ margin: 0, display:'flex' }}>{(value - (value * 0.03)).toLocaleString('pt-BR', { minimumFractionDigits: 2, style: 'currency', currency: 'BRL' })}<div className="vtex-rich-text-0-x-container vtex-rich-text-0-x-container--PIXMSG flex tl items-start justify-start t-body c-on-base"><div className="vtex-rich-text-0-x-wrapper vtex-rich-text-0-x-wrapper--PIXMSG"><p className="lh-copy vtex-rich-text-0-x-paragraph vtex-rich-text-0-x-paragraph--PIXMSG"><span className="b vtex-rich-text-0-x-strong vtex-rich-text-0-x-strong--PIXMSG">3% OFF</span> no pagamento à  <span className="b vtex-rich-text-0-x-strong vtex-rich-text-0-x-strong--PIXMSG">vista no PIX</span></p></div></div></p>
-                <p className='vtex-product-price-1-x-sellingPriceValue' style={{ fontSize: "15px", margin: '5px 0px' , color:'black', fontWeight:'bold'}}>{value.toLocaleString('pt-BR', { minimumFractionDigits: 2, style: 'currency', currency: 'BRL' })}</p>
+                <p className='vtex-product-price-1-x-sellingPriceValue' style={{ margin: 0, display: 'flex' }}>{(value).toLocaleString('pt-BR', { minimumFractionDigits: 2, style: 'currency', currency: 'BRL' })}</p>
                 {children}
-                
+                <p className='vtex-product-price-1-x-sellingPriceValue' style={{ fontSize: "15px", margin: '5px 0px', color: 'black', fontWeight: 'bold', display: 'flex', flexDirection: 'row', alignContent: 'center', justifyContent: 'center' }}>
+                    {(value - (value * 0.03)).toLocaleString('pt-BR', { minimumFractionDigits: 2, style: 'currency', currency: 'BRL' })}
+                    <div className="vtex-rich-text-0-x-container vtex-rich-text-0-x-container--PIXMSG flex tl items-start justify-start t-body c-on-base">
+                        <div className="vtex-rich-text-0-x-wrapper vtex-rich-text-0-x-wrapper--PIXMSG">
+                            <p className="lh-copy vtex-rich-text-0-x-paragraph vtex-rich-text-0-x-paragraph--PIXMSG" style={{ marginTop: '-2.5px', marginBottom: '0px' }}>
+                                <span className="b vtex-rich-text-0-x-strong vtex-rich-text-0-x-strong--PIXMSG">3% OFF</span>
+                                no pagamento à  <span className="b vtex-rich-text-0-x-strong vtex-rich-text-0-x-strong--PIXMSG">vista no PIX</span>
+                            </p>
+                        </div>
+                    </div>
+                </p>
+
                 {/* <p className='vtex-product-price-1-x-savings' style={{ margin: "0px", display: `${listValue - value == 0 ? "none" : "flex"}`, gap: "5px", flexDirection: "row" }}>Economize <strong> {(listValue - value).toLocaleString('pt-BR', { minimumFractionDigits: 2, style: 'currency', currency: 'BRL' })}</strong></p> */}
             </div>
         </>
--- tdzaindevops-dental-med-sul-io-a37ff3999229/react-app-template-master/react/components/PriceDiscountSumamry/index.tsx
+++ tdzaindevops-dental-med-sul-io-a37ff3999229/react-app-template-master/react/components/PriceDiscountSumamry/index.tsx
@@ -4,7 +4,7 @@ import { useProduct } from 'vtex.product-context'
 type props = {
     children: any
 }
-const PriceDiscountSummary: FC<props> = () => {
+const PriceDiscountSummary: FC<props> = ({ children }) => {
     const productContextValue = useProduct()
     console.log({
         Produto: productContextValue?.product,
@@ -13,7 +13,7 @@ const PriceDiscountSummary: FC<props> = () => {
     const value = productContextValue?.product?.items[0].sellers[0].commertialOffer?.Price == undefined ? 0 : productContextValue?.product?.items[0].sellers[0].commertialOffer?.Price
     // const CalcPrice = (value - (value * 0.03))
     return (
-        <div style={{display:'flex', flexDirection:'column', alignItems:'start', position:'relative', left: '-4px'}}>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'start', position: 'relative', left: '-4px' }}>
             {/* <span className="vtex-product-price-1-x-sellingPrice vtex-product-price-1-x-sellingPrice--summary vtex-product-price-1-x-sellingPrice--hasListPrice vtex-product-price-1-x-sellingPrice--summary--hasListPrice">
                 Por: <span className="vtex-product-price-1-x-sellingPriceValue vtex-product-price-1-x-sellingPriceValue--summary">
                     <span className="vtex-product-price-1-x-currencyContainer vtex-product-price-1-x-currencyContainer--summary">
@@ -21,9 +21,22 @@ const PriceDiscountSummary: FC<props> = () => {
                     </span>
                 </span>
             </span> */}
-            <div className="vtex-rich-text-0-x-container vtex-rich-text-0-x-container--PIXMSG flex tl items-start justify-start t-body c-on-base"><div className="vtex-rich-text-0-x-wrapper vtex-rich-text-0-x-wrapper--PIXMSG"><p style={{fontSize: '10px', paddingBottom:'7px'}} className="lh-copy vtex-rich-text-0-x-paragraph vtex-rich-text-0-x-paragraph--PIXMSG"><span className="b vtex-rich-text-0-x-strong vtex-rich-text-0-x-strong--PIXMSG">3% OFF</span> no pagamento à  <span className="b vtex-rich-text-0-x-strong vtex-rich-text-0-x-strong--PIXMSG">vista no PIX</span></p></div></div>
-            <p className='vtex-product-price-1-x-sellingPriceValue' style={{ fontSize: "10px", margin: '5px 5px' , color:'black'}}>{value.toLocaleString('pt-BR', { minimumFractionDigits: 2, style: 'currency', currency: 'BRL' })}</p>
-            
+            <p className='vtex-product-price-1-x-sellingPriceValue' style={{ fontSize: "17px", margin: '5px 5px', display: 'flex', flexDirection: 'column', alignItems: 'start', color: '#43aadd', }}>
+                {value.toLocaleString('pt-BR', { minimumFractionDigits: 2, style: 'currency', currency: 'BRL' })}
+                <div style={{ fontSize: '10px', color: 'black', fontWeight: 'bold', display: 'flex', flexDirection: 'column', alignItems: 'start', marginTop: '10px' }}>
+                    {children}
+                    <span style={{paddingTop:'5px'}}>{(value - (value * 0.03)).toLocaleString('pt-BR', { minimumFractionDigits: 2, style: 'currency', currency: 'BRL' })}</span>
+                    <div className="vtex-rich-text-0-x-container vtex-rich-text-0-x-container--PIXMSG flex tl items-start justify-start t-body c-on-base">
+                        <div className="vtex-rich-text-0-x-wrapper vtex-rich-text-0-x-wrapper--PIXMSG" style={{ marginLeft: '0px' }}>
+                            <p style={{ fontSize: '12px', paddingBottom: '7px' }} className="lh-copy vtex-rich-text-0-x-paragraph vtex-rich-text-0-x-paragraph--PIXMSG">
+                                <span className="b vtex-rich-text-0-x-strong vtex-rich-text-0-x-strong--PIXMSG">3% OFF </span>
+                                 no pagamento à  <span className="b vtex-rich-text-0-x-strong vtex-rich-text-0-x-strong--PIXMSG">vista no PIX</span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </p>
+
         </div>
     )
 }
